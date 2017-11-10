497a874a044c24c5034882cfabc2f29e322d60b9
gabinesh <gabineshthivakaram@hotmail.co.uk>
2017-11-10 16:13:20 +0000

    login

--- src/login.py
+++ src/login.py
@@ -7,5 +7,13 @@ def login_check(username, password):
     connect = db.connect('SCM_DATABASE.db')
     connect.row_factory = lambda cursor, row: row[0]
     database = connect.cursor()
+    username_check = database.execute('SELECT username FROM login WHERE username=?',(username,)).fetchall()#checks to see if user name stored in login table
+    password_check = database.execute('SELECT password FROM login WHERE username=?',(password,)).fetchall()#checks to see if passwored matches username
+
+    if len(username_check) == 0 or len(password_check) == 0:
+        #if no matches are found than deny access
+        return False
+    else:
+        return True#else successful
 
 
