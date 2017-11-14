d68eefb70a77b234936fc8da41588f3d6eb789ce
gabinesh <gabineshthivakaram@hotmail.co.uk>
2017-11-14 15:18:35 +0000

    delete

--- src/delete.py
+++ src/delete.py
@@ -1,11 +1,19 @@
 import sqlite3 as db #loads sqlite python module
 
 
-def prompt():
+def prompt(code):
     """function that takes user input item code to generate and return
        item details for notification purposes"""
     
     connect = db.connect('SCM_DATABASE.db')
     connect.row_factory = lambda cursor, row: row[0]
     database = connect.cursor()
+
+    codeReturn = database.execute('SELECT code FROM stock WHERE code =?',(code,)).fetchall()
+    nameReturn = database.execute('SELECT name FROM stock WHERE code =?',(code,)).fetchall()
+    quantityReturn = database.execute('SELECT quantity FROM stock WHERE code =?',(code,)).fetchall()
+
+    
+    return[codeReturn[0],nameReturn[0],quantityReturn[0]]#details for notification purposes
+
 
