50367a2858a12fac902830f0d8be14a808a7957e
gabinesh <gabineshthivakaram@hotmail.co.uk>
2017-11-14 15:20:35 +0000

    Load Trend

--- src/viewSales.py
+++ src/viewSales.py
@@ -1,8 +1,12 @@
 import sqlite3 as db
 
-def loadTrend():
+def loadTrend(code):
     """function which gets trend status according to item code inputted"""
     connect = db.connect('SCM_DATABASE.db')
     connect.row_factory = lambda cursor, row: row[0]
     database = connect.cursor()
+
+    trend = database.execute('SELECT salesTrend FROM sales WHERE code=?',(code,)).fetchall()
+    return trend[0]#returns item codes sale trend
+
 
