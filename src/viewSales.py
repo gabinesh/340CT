6137c3e39d8a65d89efede4431b07166bc8f6b53
gabinesh <gabineshthivakaram@hotmail.co.uk>
2017-11-19 09:21:23 +0000

    Load Stock

--- src/viewSales.py
+++ src/viewSales.py
@@ -15,3 +15,19 @@ def loadStock():
     connect = db.connect('SCM_DATABASE.db')
     connect.row_factory = lambda cursor, row: row[0]
     database = connect.cursor()
+
+    code = database.execute('SELECT code FROM stock').fetchall()
+    name = database.execute('SELECT name FROM stock').fetchall()
+    quantity = database.execute('SELECT quantity FROM stock').fetchall()
+
+    trend = []#list that stores items sales trend status
+
+    for i in range(0,len(code)):
+        """for each element in code list(holds all item codes)
+            get that item codes trend sales status"""
+        salesTrendForItem = loadTrend(code[i])
+        trend.append(salesTrendForItem)
+        
+
+
+    return[code,name,quantity,trend]#sends all details back to gui for final notificaion
